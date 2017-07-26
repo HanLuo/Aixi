5261740cecde66d6c4288d2a07f075a17ecdb3b0
shenwei <1903851304@qq.com>
2017-07-27 05:23:46 +0800

    add

---
+++ Server/GameServer/Player.h
@@ -0,0 +1,21 @@
+#/*!
+ * \class Player
+ *
+ * \brief 指已经通过网络连接进入服务器端程序的客户
+ *
+ * \author shenwei
+ * \date 七月 2017
+ */
+
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include "Type.h"
+#include "Socket.h"
+#include "SocketInputStream.h"
+#include "SocketOutputStream.h"
+#include "Packet.h"
+
+
+
+#endif
